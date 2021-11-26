3900f99e7dd49f217065a97557263d2b1d40e428
Priyansh_Diwan <priyanshdiwan2@gmail.com>
2021-11-27 00:11:07 +0530

    deleted some useless fiels

--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-
-print(x.difference_update(y))
-# ye error de dega kyuki diffrence update mai diffrence/intersection jo hoga x aurb y ka
-#  vo x mai ja ke save ho jayega to print(x command dena padega)
-print(x)
